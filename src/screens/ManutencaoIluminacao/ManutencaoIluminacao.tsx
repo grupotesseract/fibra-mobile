56d008d393d10a583ec1bca27f445f789aa0ac8e
Evandro Carreira <evandro.carreira@gmail.com>
2019-10-07 20:18:43 -0300

    feat(lista-itens): Mockup de listagem de itens
    Tela de listagem de itens com botão de demonstração para leitura de
    QRCode e finalização da amnutenção
    
    Closes #168701353

--- src/screens/ManutencaoIluminacao/ManutencaoIluminacao.tsx
+++ src/screens/ManutencaoIluminacao/ManutencaoIluminacao.tsx
@@ -1,45 +1,57 @@
 import React, { Component } from 'react';
-import { Container, Content, Button, Text, Fab, Card, CardItem, Body, Input, Item, Label } from 'native-base';
+import { Container, Content, Button, Text, List, ListItem, Left, Right, Icon, Grid, Row, Badge } from 'native-base';
 import HeaderNav from '../../components/HeaderNav';
-import { ScrollView, KeyboardAvoidingView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
-const materiais = [
+const itens = [
     {
         id:1,
-        tipo: 'FL',
-        nome: 'Fluorescente FL',
-        tensao: '227V',
-        potencia: '150W',
-        quantidade: '20'
+        nome: 'Item 1 - Hall de entrada'
     },
     {
         id:2,
-        tipo: 'FL',
-        nome: 'Fluorescente FL',
-        tensao: '110V',
-        potencia: '150W',
-        quantidade: ''
+        nome: 'Item 2 - Salão do maquinário principal'
     },
     {
         id:3,
-        tipo: 'FL',
-        nome: 'Fluorescente FL',
-        tensao: '227V',
-        potencia: '50W',
-        quantidade: ''
+        nome: 'Item 3 - Banheiro do salão'
     },
     {
         id:4,
-        tipo: 'FL',
-        nome: 'Fluorescente FL',
-        tensao: '227V',
-        potencia: '150W',
-        quantidade: null
-    }
+        nome: 'Item 4 - Atendimento ao cliente'
+    },
+    {
+        id:5,
+        nome: 'Item 5 - Salão do maquinário'
+    },
+    {
+        id:13,
+        nome: 'Item 3 - Banheiro do salão'
+    },
+    {
+        id:14,
+        nome: 'Item 4 - Atendimento ao cliente'
+    },
+    {
+        id:15,
+        nome: 'Item 5 - Salão do maquinário'
+    },
+    {
+        id:23,
+        nome: 'Item 3 - Banheiro do salão'
+    },
+    {
+        id:24,
+        nome: 'Item 4 - Atendimento ao cliente'
+    },
+    {
+        id:25,
+        nome: 'Item 5 - Salão do maquinário'
+    },
 ]
 export default class ManutencaoIluminacao extends Component {
     state = {
-        materiais
+        itens
     }
 
     render() {
@@ -47,41 +59,39 @@ export default class ManutencaoIluminacao extends Component {
             <Container>
                 <HeaderNav title="Manutenção Iluminação"  />
                 <Content padder contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
-                    <KeyboardAvoidingView
-                        behavior="height"
-                    >
-                        <ScrollView>
+                    <ScrollView>
+                        <List>
                             {
-                                this.state.materiais.map(material => {
-                                    return <Card key={material.id}>
-                                        <CardItem header bordered>
-                                            <Text>{material.nome}</Text>
-                                        </CardItem>
-                                        <CardItem>
-                                            <Body>
-                                                <Text>Tipo: {material.tipo}</Text>
-                                                <Text>Potência: {material.potencia}</Text>
-                                                <Text>Tensão: {material.tensao}</Text>
-                                            </Body>
-                                        </CardItem>
-                                    </Card>
+                                this.state.itens.map(item => {
+                                    return <ListItem key={item.id}>
+                                            <Left>
+                                                <Text>{ item.nome }</Text>
+                                            </Left>
+                                            <Right>
+                                                <Badge success>
+                                                    <Text>ok</Text>
+                                                </Badge>
+                                            </Right>
+                                        </ListItem>
                                 })
                             }
-                        <Button
-                            block
-                            onPress={() => this.props.navigation.navigate('MenuVistoria')}
-                            style={style.btnStyle}
-                            disabled={!this.state.materiais.reduce( (tudoPreenchido, material) => {
-                                return tudoPreenchido 
-                                        && material.quantidade  !== null
-                                        && material.quantidade  !== ''
-                            }, true)}
-                        >
-                            <Text>Concluído</Text>
-                        </Button>
-                        </ScrollView>
-                    </KeyboardAvoidingView>
+                        
+                        </List>
+                    </ScrollView>
                 </Content>
+                <View style={{ flexDirection: 'row' }}>
+                    <Button style={style.botaoQuadrado}>
+                        <Icon name='md-qr-scanner' style={{fontSize: 48}}/>
+                        <Text>LER QRCODE</Text>
+                    </Button>
+                    <Button style={style.botaoQuadrado}>
+                        <Icon name='md-checkmark' style={{fontSize: 36}} />
+                        <View style={{alignItems: 'center'}}>
+                            <Text>CONCLUIR</Text>
+                            <Text>MANUTENÇÃO</Text>
+                        </View>
+                    </Button>
+                </View>
             </Container>
         );
     }
@@ -90,5 +100,14 @@ export default class ManutencaoIluminacao extends Component {
 const style = {
     btnStyle: {
         marginVertical: 5,
+    },
+    botaoQuadrado: {
+        margin: 5,
+        padding: 10,
+        paddingBottom: 10,
+        height: 110,
+        flex: 1,
+        justifyContent: 'space-evenly',
+        flexDirection: "column"
     }
 }
