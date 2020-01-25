e52468622dc56e586cd242d93f70a98c450c8419
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-01-24 23:44:01 -0300

    Alterando mensagem para caso qrcode não é encontrado

--- src/components/QRCodeReader.tsx
+++ src/components/QRCodeReader.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, Button, ActivityIndicator } from 'react-native';
 import { View } from "native-base";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { TouchableHighlight, Dimensions } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+  
 
 export function QRCodeReader(props) {
     
@@ -11,7 +12,7 @@ export function QRCodeReader(props) {
 
     const handleBarCodeScanned = ({ type, data }) => {
         setScanned(true);
-        alert(`QRCODE ${data} ENCONTRADO. FAVOR AGUARDAR O ITEM SER CARREGADO`);
+        alert(`QRCODE ${data} ESCANEADO. FAVOR AGUARDAR!!`);
         const { navigation } = props;
         const qrcode = data;
         navigation.navigate({ routeName: 'ManutencaoItem', params: { qrcode }})
@@ -20,21 +21,39 @@ export function QRCodeReader(props) {
 
     var {height} = Dimensions.get('window');
 
-    return <View style={{zIndex:9, position: 'absolute', backgroundColor: 'black', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'flex-start', alignItems: 'flex-start', height: height}}>
-        <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-        />
-
-        {/* <TouchableHighlight 
-        onPress={() => props.handleClose}
-        style={{ zIndex: 10, alignSelf: 'flex-start', position: 'absolute', top: 0, left: 10, padding: 10}}
-        >
-        <FontAwesome
-            name="arrow-left"
-            size={36}
-            color='white'            
+    
+    return (
+    
+        <View style={{zIndex:9, position: 'absolute', backgroundColor: 'black', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'flex-start', alignItems: 'flex-start', height: height}}>
+            
+            <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
             />
-        </TouchableHighlight> */}
-    </View>
+
+            {/* <TouchableHighlight 
+            onPress={() => props.handleClose}
+            style={{ zIndex: 10, alignSelf: 'flex-start', position: 'absolute', top: 0, left: 10, padding: 10}}
+            >
+            <FontAwesome
+                name="arrow-left"
+                size={36}
+                color='white'            
+                />
+            </TouchableHighlight> */}
+        </View>
+    );
+
+    
 }
+
+const styles = {
+    container: {
+      flexGrow: 1
+    },
+    spinner: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  };
