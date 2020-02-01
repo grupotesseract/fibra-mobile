0e133a41314a32191d36b5ab12a87fa800b5346a
User Name <dev@email.com>
2020-02-01 14:56:46 -0300

    feat: (WIP) Melhorias no HeaderLogo

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -1,6 +1,34 @@
 import React from 'react'
-import { Header, Body, View } from 'native-base'
+import { Header, Body, View, Left, Right, Button, Icon } from 'native-base'
 import { StyleSheet } from 'react-native'
+import Logo from './Logo'
+
+const HeaderLogo = () => {
+  return (
+    <View>
+      <Header transparent>
+        <Left>
+          <Button onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+            <Icon name="menu" />
+          </Button>
+          {/* <Button
+            transparent
+            onPress={() => this.logoff()}
+            style={style.btnStyle}
+          >
+            <Icon name="exit" />
+          </Button> */}
+        </Left>
+
+        <Body>
+          <Logo size="xs" />
+        </Body>
+      </Header>
+    </View>
+  )
+}
+
+export default HeaderLogo
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +38,8 @@ const styles = StyleSheet.create({
   }
 })
 
-const HeaderLogo = () => {
-  return (
-    <View style={styles.container}>
-      <Header transparent style={{ backgroundColor: 'white' }}>
-        <Body>
-        </Body>
-      </Header>
-    </View>
-  )
+const style = {
+  btnStyle: {
+    marginVertical: 5,
+  }
 }
-
-export default HeaderLogo
