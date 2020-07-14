fac1f48de44fc305878d3fededcc27d36891246b
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-07-14 08:42:43 -0300

    Mudando versão

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -65,7 +65,7 @@ class Login extends Component<Props, State> {
         <View padder>
           <Logo center size="md" />
 
-          <Text style={style.text}>1.2.0</Text>
+          <Text style={style.text}>1.5.0</Text>
 
           <Form style={style.form}>
             <Item stackedLabel>
