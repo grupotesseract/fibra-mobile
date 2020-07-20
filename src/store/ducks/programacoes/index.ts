42327f5d212297b1de83def4db531ae28f1b24cf
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-07-20 17:58:20 -0300

    Removendo console.log

--- src/store/ducks/programacoes/index.ts
+++ src/store/ducks/programacoes/index.ts
@@ -358,7 +358,6 @@ const programacoesReducer: Reducer<ProgramacoesState> = (state = INITIAL_STATE,a
         {
             const { idProgramacao, idItem, materiaisAlterados } = action.payload;
             const { programacoesRealizadas } = state;
-            console.log(action.payload);
             return {
                 ...state,
                 programacoesRealizadas: programacoesRealizadas.map(programacaoRealizada => {
