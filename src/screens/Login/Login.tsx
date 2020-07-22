0d3e70c64af34a3fe6f9db9d997d172792207339
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-07-22 10:19:45 -0300

    Mudando a versão pra 2.0.0

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -67,7 +67,7 @@ class Login extends Component<Props, State> {
         <View padder>
           <Logo center size='xlg' />
 
-          <Text style={style.text}>1.5.0</Text>
+          <Text style={style.text}>2.0.0</Text>
 
           <Form style={style.form}>
             <Item stackedLabel>
